a5add9b468e7d0e9dfe146b832fdfe520ecb56a3
Mark Strendin <mark.strendin@gmail.com>
2020-08-10 22:53:13 -0600

    Put the deprovision user function in the deprovision script for now. It doesn't make sense to "functionize" the others because of their complexity.

--- Include/ADFunctions.ps1
+++ Include/ADFunctions.ps1
@@ -36,29 +36,3 @@ function Get-SyncableEmployeeIDs {
     return $employeeIDs
 }
 
-function Deprovision-User 
-{
-    param(
-        [Parameter(Mandatory=$true)] $Identity,
-        [Parameter(Mandatory=$true)][String] $EmployeeType,
-        [Parameter(Mandatory=$true)][String] $DeprovisionOU
-    )
-
-    Write-Log "Deprovisioning: $EmployeeId ($($ADUser))"
-
-    $DepTime = Get-Date  
-    set-aduser $Identity -Description "Deprovisioned: $DepTime" -Enabled $true -Department "$DeprovisionedEmployeeType" -Office "$DeprovisionedEmployeeType" -Replace @{'employeeType'="$DeprovisionedEmployeeType";'title'="$DeprovisionedEmployeeType"}
-
-    # Remove all group memberships
-    foreach($Group in Get-ADPrincipalGroupMembership -Identity $Identity)
-    {
-        # Don't remove from "domain users", because it won't let you do this anyway (its the user's "default group").
-        if ($Group.Name -ne "Domain Users")
-        {
-            Remove-ADGroupMember -Identity $Group -Members $Identity -Confirm:$false
-        }
-    }
-
-    # Move user to deprovision OU
-    move-ADObject -identity $Identity -TargetPath $DeprovisionOU 
-}
