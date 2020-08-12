e243d6fa22a943b08667471b38ee647a5981d966
Mark Strendin <mark.strendin@lskysd.ca>
2020-08-12 14:45:33 -0600

    Add logging, and try catch blocks in scripts. (#3)
    It's not ideal to encapsulate the entire file in a try catch, but this
    way the script actually ends after the first exception, rather than
    iterating through 5000 failed attempts to do something.

--- sis-export/export-students-schoollogic.ps1
+++ sis-export/export-students-schoollogic.ps1
@@ -44,39 +44,48 @@ $QuoteAllColumns = $false
 # No configurable settings beyond this point #
 ##############################################
 
-# Find the config file
-$AdjustedConfigFilePath = $ConfigFile
-if ($AdjustedConfigFilePath.Length -le 0) {
-    $AdjustedConfigFilePath = join-path -Path $(Split-Path (Split-Path $MyInvocation.MyCommand.Path -Parent) -Parent) -ChildPath "config.xml"
-}
-
-# Retreive the connection string from config.xml
-if ((test-path -Path $AdjustedConfigFilePath) -eq $false) {
-    Throw "Config file not found. Specify using -ConfigFilePath. Defaults to config.xml in the directory above where this script is run from."
-}
-$configXML = [xml](Get-Content $AdjustedConfigFilePath)
-$ConnectionString = $configXML.Settings.ConnectionStrings.SchoolLogic
-
-# Set up the SQL connection
-$SqlConnection = new-object System.Data.SqlClient.SqlConnection
-$SqlConnection.ConnectionString = $ConnectionString
-$SqlCommand = New-Object System.Data.SqlClient.SqlCommand
-$SqlCommand.CommandText = $SqlQuery
-$SqlCommand.Connection = $SqlConnection
-$SqlAdapter = New-Object System.Data.SqlClient.SqlDataAdapter
-$SqlAdapter.SelectCommand = $SqlCommand
-$SqlDataSet = New-Object System.Data.DataSet
-
-# Run the SQL query
-$SqlConnection.open()
-$SqlAdapter.Fill($SqlDataSet)
-$SqlConnection.close()
-
-# Output to a CSV file
-foreach($DSTable in $SqlDataSet.Tables) {
-    if (($PSVersionTable.PSVersion.Major -ge 7) -and ($QuoteAllColumns -eq $false)) {
-        $DSTable | export-csv $OutFile -notypeinformation -Delimiter $Delimeter -UseQuotes AsNeeded
-    } else {
-        $DSTable | export-csv $OutFile -notypeinformation -Delimiter $Delimeter
+. ./../Include/UtilityFunctions.ps1
+Write-Log "Starting SIS exportscript..."
+try {
+    # Find the config file
+    $AdjustedConfigFilePath = $ConfigFile
+    if ($AdjustedConfigFilePath.Length -le 0) {
+        $AdjustedConfigFilePath = join-path -Path $(Split-Path (Split-Path $MyInvocation.MyCommand.Path -Parent) -Parent) -ChildPath "config.xml"
     }
+
+    # Retreive the connection string from config.xml
+    if ((test-path -Path $AdjustedConfigFilePath) -eq $false) {
+        Throw "Config file not found. Specify using -ConfigFilePath. Defaults to config.xml in the directory above where this script is run from."
+    }
+    $configXML = [xml](Get-Content $AdjustedConfigFilePath)
+    $ConnectionString = $configXML.Settings.ConnectionStrings.SchoolLogic
+
+    # Set up the SQL connection
+    $SqlConnection = new-object System.Data.SqlClient.SqlConnection
+    $SqlConnection.ConnectionString = $ConnectionString
+    $SqlCommand = New-Object System.Data.SqlClient.SqlCommand
+    $SqlCommand.CommandText = $SqlQuery
+    $SqlCommand.Connection = $SqlConnection
+    $SqlAdapter = New-Object System.Data.SqlClient.SqlDataAdapter
+    $SqlAdapter.SelectCommand = $SqlCommand
+    $SqlDataSet = New-Object System.Data.DataSet
+
+    # Run the SQL query
+    $SqlConnection.open()
+    $SqlAdapter.Fill($SqlDataSet)
+    $SqlConnection.close()
+
+    # Output to a CSV file
+    foreach($DSTable in $SqlDataSet.Tables) {
+        if (($PSVersionTable.PSVersion.Major -ge 7) -and ($QuoteAllColumns -eq $false)) {
+            $DSTable | export-csv $OutFile -notypeinformation -Delimiter $Delimeter -UseQuotes AsNeeded
+        } else {
+            $DSTable | export-csv $OutFile -notypeinformation -Delimiter $Delimeter
+        }
+    }
+    
 }
+catch { 
+    Write-Log $_
+}
+Write-Log "Finished SIS export script."
