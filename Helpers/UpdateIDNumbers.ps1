a1775d7d232f482777acbddadd325767ba7c0127
Mark Strendin <mark.strendin@lskysd.ca>
2021-08-26 15:40:28 -0600

    ID number migration script works

--- Helpers/UpdateIDNumbers.ps1
+++ Helpers/UpdateIDNumbers.ps1
@@ -16,11 +16,17 @@ import-module ActiveDirectory
  foreach($Row in $ImportfileRows) {
     if ($Row."Legacy ID" -ne "") 
     {        
-        write-host "Old:" $Row."Legacy ID" "New:" $Row."Pupil #"
-        $ID = $Row."Legacy ID"
-        $User = Get-ADUser -Filter "employeeid -eq $ID"
-        write-host $User
+        #write-host "Old:" $Row."Legacy ID" "New:" $Row."Pupil #"
+        $LegacyID = $Row."Legacy ID"
+        $NewID = $Row."Pupil #"
+        $User = Get-ADUser -Filter "(employeeid -eq $LegacyID) -and (employeetype -eq 'Student')" -Properties cn,postOfficeBox,givenName,sn
+        
+        if ($null -ne $User) {
+         write-host $User
+         $NewCN = $User.givenName + " " + $User.sn + " (" + $NewID + ")"
+         $User | Set-AdUser -POBox $LegacyID -EmployeeID $NewID        
+         $User | Rename-ADObject -NewName $NewCN         
+        }
     }
-    
  }
  
