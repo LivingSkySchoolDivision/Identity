2c1e53f1256eb50d0bb75d9524882ece714ea06a
Mark Strendin <mark.strendin@gmail.com>
2022-05-31 13:57:47 -0600

    Fix cleanup task script, clean up cleanup script

--- Tasks/Cleanup.ps1
+++ Tasks/Cleanup.ps1
@@ -1,27 +1,9 @@
 param (
-    [Parameter(Mandatory=$true)][string]$FacilityFile,
     [Parameter(Mandatory=$true)][string]$ConfigFile,
-    [Parameter(Mandatory=$true)][string]$LogDirectory
+    [Parameter(Mandatory=$true)][string]$LogFilePath
 )
 
 $JobNameNoSpaces = "IdentityCleanup"
-
-
-## #########################################################
-## # Set script location so the relative paths all work
-## #########################################################
-
-$OldLocation = get-location
-set-location $PSScriptRoot
-
-## #########################################################
-## # Normalize input paths
-## #########################################################
-
-$ConfigFile = $(Resolve-Path $ConfigFile)
-$FacilityFile = $(Resolve-Path $FacilityFile)
-$LogFilePath = $(Resolve-Path $LogFilePath)
-$InputFile = $(Resolve-Path $InputFile)
 
 ## #########################################################
 ## # Functions
@@ -35,9 +17,7 @@ function Write-Log
 
     Write-Output "$(Get-Date -Format "yyyy-MM-dd HH:mm:ss K")> $Message"
 }
-## #########################################################
-## # Set up a filename for the logs
-## #########################################################
+
 function Get-FullTimeStamp 
 {
     $now=get-Date
@@ -50,24 +30,59 @@ function Get-FullTimeStamp
     return $timestamp
 }
 
-$LogFile = Join-Path $LogDirectory ((Get-FullTimeStamp) + "-$JobNameNoSpaces.log")
-if ((Test-Path $LogDirectory) -eq $false) {
-    Write-Log "Creating log file directory at $LogDirectory"
-    New-Item -Path $LogDirectory -ItemType Directory
+## #########################################################
+## # Normalize input paths
+## #########################################################
+
+$OldLocation = Get-Location
+$NormalizedConfigFile = $(Resolve-Path $ConfigFile)
+$NormalizedLogFilePath = $(Resolve-Path $LogFilePath)
+Set-Location $PSScriptRoot
+$NormalizedScriptRoot = Resolve-Path "../Tasks-Students"
+Set-Location $OldLocation
+
+Write-Log "Working Directory is: $OldLocation"
+Write-Log "Using config file: $NormalizedConfigFile"
+Write-Log "Using log file path: $NormalizedLogFilePath"
+Write-Log "Using script root: $NormalizedScriptRoot"
+
+## #########################################################
+## # Set up a filename for the logs
+## #########################################################
+
+$LogFile = Join-Path $LogFilePath ((Get-FullTimeStamp) + "-$JobNameNoSpaces.log")
+if ((Test-Path $LogFilePath) -eq $false) {
+    Write-Log "Creating log file directory at $LogFilePath"
+    New-Item -Path $LogFilePath -ItemType Directory
 }
 
 Write-Host "Logging to $LogFile"
 Write-Log "Starting $JobNameNoSpaces script..." >> $LogFile
+
 ## #########################################################
 ## # Checks to make sure necesary files exist
 ## #########################################################
 
 # Make sure config file exists
-if ((Test-Path $ConfigFilePath) -eq $false) {
+if ((Test-Path $NormalizedConfigFile) -eq $false) {
     Write-Log "Config file not found. exiting." >> $LogFile
     Write-Log "Finished full sync script with errors." >> $LogFile
     set-location $OldLocation
     exit
+}
+
+## #########################################################
+## # Run cleanup script...
+## #########################################################
+
+Write-Log "Calling cleanup script..." >> $LogFile
+try {
+    $ScriptPath = Join-Path -Path $NormalizedScriptRoot -ChildPath "Ident-Cleanup.ps1"
+    powershell -NoProfile -File $ScriptPath -ConfigFile $NormalizedConfigFile >> $LogFile
+} 
+catch {
+    Write-Log "Exception running move/update script."
+    Write-Log $_
 }
 
 ## #########################################################
