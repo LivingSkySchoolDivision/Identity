d868a692a852ffa4db99ad5fc314000ba4c074c6
Mark Strendin <mark.strendin@lskysd.ca>
2022-09-28 15:00:05 -0600

    Fix for the script getting stuck at a prompt while removing AD users.

--- Tasks-Students/Ident-Cleanup.ps1
+++ Tasks-Students/Ident-Cleanup.ps1
@@ -52,7 +52,7 @@ try {
     foreach($User in Get-ADUser -filter {Enabled -eq $false -AND employeeType -eq $DeprovisionedEmployeeType -AND whenChanged -lt $PurgeCutoffDay} -Properties whenChanged)
     {
         Write-Log "PURGE: $($User.DistinguishedName)"
-        Remove-ADUser -Identity $User
+        Remove-ADUser -Confirm:$False -Identity $User
     }
 
     # Find any currently enabled accounts that have been untouched for the given amount of days
